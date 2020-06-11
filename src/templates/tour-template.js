874938b1704e0a7819d22f4212fdef33cb9dcad4
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-06-11 22:30:58 +0200

    Add single-tour page for each tour

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,7 +1,96 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import { FaMoneyBillWave, FaMap } from "react-icons/fa"
 
-const tourTemplate = () => {
-  return <div>hello from dummy data template</div>
+import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import Day from "../components/SingleTour/Day"
+
+import styles from "../css/template.module.css"
+
+const tourTemplate = ({ data }) => {
+  const {
+    name,
+    price,
+    country,
+    days,
+    start,
+    description: { description },
+    images,
+    journey,
+  } = data.tour
+  const [mainImage, ...tourImages] = images
+
+  return (
+    <Layout>
+      <StyledHero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {tourImages.map((item, idx) => {
+              return (
+                <Img
+                  key={idx}
+                  fluid={item.fluid}
+                  alt="single tour"
+                  className={styles.image}
+                />
+              )
+            })}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              starting from ${price}
+            </p>
+            <p>
+              <FaMap className={styles.icon} />
+              {country}
+            </p>
+          </div>
+          <h4>starts on: {start}</h4>
+          <h4>duration: {days} days</h4>
+          <p className={styles.desc}>{description}</p>
+          <h2>daily schedule</h2>
+          <div className={styles.journey}>
+            {journey.map((item, idx) => {
+              return <Day key={idx} day={item.day} info={item.info} />
+            })}
+          </div>
+          <AniLink fade to="/tours" className="btn-primary">
+            back to tours
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    tour: contentfulTour(slug: { eq: $slug }) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description {
+        description
+      }
+      journey {
+        day
+        info
+      }
+      images {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
 
 export default tourTemplate
